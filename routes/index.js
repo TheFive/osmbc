fe9c64f6ae7583eca140fefd9947f54834d946ed
TheFive <TheFive.osm@gmail.com>
2015-08-07 19:25:22 +0200

    Changed index to production index name.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' ,           user:req.user,});
 });
 router.get('/osmbc.html', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' ,           user:req.user,});
 });
 
 module.exports = router;
