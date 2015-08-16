ae93a89c2be28675988a9777adb59b9b3a615008
TheFive <TheFive.osm@gmail.com>
2015-08-16 13:08:13 +0200

    Upload Code Coverage only from Travis, introduced util test.

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -n "TRAVIS" ]; then
+istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/codecov.io/bin/codecov.io.js && rm -rf ./coverage 
+
+
+elif true; then
+
+istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info 
+
+fi
