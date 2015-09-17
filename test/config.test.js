87bdffd851d66fc788fceb1ae23c6a54f36ca781
TheFive <TheFive.osm@gmail.com>
2015-09-17 09:04:50 +0200

    Changed Test

--- test/config.test.js
+++ test/config.test.js
@@ -14,14 +14,14 @@ describe('config',function() {
   })
   describe("get*",function() {
     it('should return standard values',function(bddone){
-      should((config.getCallbackUrl())).equal("TestValue");
+      should((config.getCallbackUrl())).equal("http://localhost:3000/auth/openstreetmap/callback");
       should((config.getServerPort())).equal(3000);
       bddone();
     })
   })
   describe("getValue",function() {
     it('should return Standard Values',function(bddone){
-      should((config.getValue("callbackUrl"))).equal("TestValue");
+      should((config.getValue("callbackUrl"))).equal("http://localhost:3000/auth/openstreetmap/callback");
       should((config.getValue("serverport"))).equal(3000);
       bddone();
     })
