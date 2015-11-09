95bdaeaf3298b345fe2981f52214eab46199703d
TheFive <TheFive.osm@gmail.com>
2015-11-09 08:12:01 +0100

    Quickfix Error Handler.

--- app.js
+++ app.js
@@ -250,7 +250,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
+    layout:{htmlroot:htmlRoot}
   });
 });
 
