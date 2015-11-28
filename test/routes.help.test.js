a2d94d490d9d998645cca8afb42b35bf71223d79
TheFive <TheFive.osm@gmail.com>
2015-11-28 14:09:54 +0100

    test

--- test/routes.help.test.js
+++ test/routes.help.test.js
@@ -7,9 +7,9 @@ var helpRoutes = require('../routes/help.js');
 
 var testutil = require('./testutil.js');
 
-describe('routes/help',function(){
+describe.only('routes/help',function(){
   describe('generateHelpText',function(){
-    it('should render the text with token',function(){
+    it.only:('should render the text with token',function(){
       var result = helpRoutes.getText('../test/data/helptext.test.md');
       should(result).equal('<h2>This Is a Help Test</h2>\n<p>Use a <a href="/testlink">link</a> to test tokens.</p>\n');
     })
@@ -17,3 +17,4 @@ describe('routes/help',function(){
 })
 
 
+ 
