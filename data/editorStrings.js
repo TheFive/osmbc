d932d0383131504848e3a2ed50bd2922d5c8ba1f
TheFive <TheFive.osm@gmail.com>
2015-12-26 19:11:59 +0100

    Change Editor Strings. Close #223

--- data/editorStrings.js
+++ data/editorStrings.js
@@ -2,16 +2,16 @@ var weeklyDesign = '<p align="right"><i>##text##</i></p>';
 var wnDesign = weeklyDesign;
 
 var text = {
-  "DE":"Diese Wochennotiz wurde erstellt von ##team##.",
-  "EN":"This weekly was produced by ##team##.",
-  "ES": "This weekly was produced by ##team##.",
-  "FR": "This weekly was produced by ##team##.",
-  "CZ": "This weekly was produced by ##team##.",
-  "PT": "This weekly was produced by ##team##.",
-  "RU": "This weekly was produced by ##team##.",
-  "JP": "This weekly was produced by ##team##.",
-  "ID": "This weekly was produced by ##team##.",
-  "TR": "This weekly was produced by ##team##.",
+  "DE": "Diese Wochennotiz wurde erstellt von ##team##.",
+  "EN": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "ES": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "FR": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "CZ": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "PT": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "RU": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "JP": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "ID": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
+  "TR": "This weekly was <a href=\"https://wiki.openstreetmap.org/wiki/WeeklyOSM#Languages\">produced</a> by ##team##.",
 };
 
 var designedText = {};
