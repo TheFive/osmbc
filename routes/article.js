0538df647db919a36eeb4dbbde43e9563230b383
TheFive <TheFive.osm@gmail.com>
2016-03-06 13:45:09 +0100

    Explanation. Avoid unnecessary load of collectors to speed up.

--- routes/article.js
+++ routes/article.js
@@ -364,7 +364,7 @@ function postSetMarkdown(req, res, next) {
       change.old["markdown"+lang]=oldMarkdown;
       article.setAndSave(req.user,change,function(err){
         if (err) return next(err);
-        var returnToUrl = config.getValue('htmlroot')+"/blog/"+article.blog+"/previewNEdit?lang="+lang;
+        var returnToUrl = config.getValue('htmlroot')+"/blog/"+article.blog+"/previewNEdit";
         res.redirect(returnToUrl);
       });
     }
