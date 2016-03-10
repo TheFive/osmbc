d61d1271f0eccf9eba90b78d6deae17d65cead85
TheFive <TheFive.osm@gmail.com>
2016-03-10 21:42:48 +0100

    Test Email Validation.

--- test/views.user.js
+++ test/views.user.js
@@ -50,7 +50,7 @@ describe('views/user', function() {
     this.timeout(6000);
     nock('https://blog.openstreetmap.de')
       .get("/blog/author/WNAuthor")
-      .reply(200,"<title>WNPublic</title>");
+      .reply(200,'<meta charset="UTF-8" /> \n<title>WNPublic | OSMBlog</title>\n');
     async.series([
       function visitUser (cb) {
         browser.visit('/usert/create', cb);
@@ -75,4 +75,48 @@ describe('views/user', function() {
     });
   });
 
+  it('should not validate a usermail if wrong user logged in' ,function(bddone) {
+    this.timeout(6000);
+    async.series([
+      function createUser(cb) {
+        userModule.createNewUser({OSMUser:"TestValidate",emailInvalidation:"test@test.org",emailValidationKey:"123456789"},cb);
+      },
+      function visitUser (cb) {
+        browser.visit('/usert/2?validation=123456789', cb);
+      }
+    ],function(err){
+
+      should.exist(err);
+      should(browser.html("body")).match(/Wrong User: expected &gt;TestValidate&lt; given &gt;TheFive&lt;/);
+      bddone();
+    });
+  });
+  it('should validate a usermail if correct user logged in' ,function(bddone) {
+    this.timeout(6000);
+
+    async.series([
+      function createUser(cb) {
+        userModule.find({OSMUser:"TheFive"},function(err,result){
+          should.not.exist(err);
+          var user = result[0];
+          user.emailInvalidation = "test@test.org";
+          user.emailValidationKey="123456789";
+          user.save(cb);
+        });
+      },
+      function visitUser (cb) {
+        browser.visit('/usert/1?validation=123456789', cb);
+      }
+    ],function(err){
+
+      should.not.exist(err);
+      userModule.findById(1,function(err,result) {
+        should.not.exist(err);
+        should(result.OSMUser).eql("TheFive");
+        should(result.email).eql("test@test.org");
+        should.not.exist(result.emailInvalidation);
+        bddone();
+      });
+    });
+  });
 });
