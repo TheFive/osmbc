482ca65bd1b7fb55c113b3da6978a17adae29fea
TheFive <TheFive.osm@gmail.com>
2016-03-15 08:53:09 +0100

    Display only changes in article table for user count.

--- model/user.js
+++ model/user.js
@@ -56,7 +56,7 @@ User.prototype.calculateChanges = function calculateChanges(callback) {
   debug('User.prototype.calculateChanges');
   var self=this;
   if (self._countChanges) return;
-  pgMap.count("select count(*) as count from changes where data->>'user'='"+this.OSMUser+"'",function(err,result){
+  pgMap.count("select count(*) as count from changes where data->>'user'='"+this.OSMUser+"' and data->>'table'='article'",function(err,result){
     if (err) return callback(err);
     self._countChanges = result.count;
     return callback();
