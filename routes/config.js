854cb59453b4593ef77fe17468f40a2f68dc46a8
TheFive <TheFive.osm@gmail.com>
2016-05-18 07:43:30 +0200

    Some tests for config router.

--- routes/config.js
+++ routes/config.js
@@ -54,6 +54,8 @@ function renderConfigName(req, res, next) {
       configModule.getConfigObject(name,function(err,result){
         if (err) return cb(err);
         config = result;
+        // JSON is not initially saved, so create it by getting it.
+        config.json = config.getJSON();
         return cb();
       });
     },
@@ -80,7 +82,6 @@ function renderConfigName(req, res, next) {
       if (name == "categorytranslation") jadeFile = name;
       if (name == "automatictranslatetext") jadeFile = name;
 
-      console.dir(config);
       res.render(jadeFile,{config:config,
                         changes:changes,
                         params:params,
