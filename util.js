7672aa834f4cf69a54320572c6d218616442c96d
TheFive <TheFive.osm@gmail.com>
2017-01-21 18:59:30 +0100

    Linting the next.

--- util.js
+++ util.js
@@ -1,63 +1,63 @@
 "use strict";
 
-var debug = require ('debug')('OSMBC:util');
-var markdown = require('markdown-it')({breaks:true});
+var debug = require("debug")("OSMBC:util");
+var markdown = require("markdown-it")({breaks: true});
 
 
 function isTrue(expr) {
   return (expr === true || expr === "true");
 }
 
-function shorten(string,maxlength) {
+function shorten(string, maxlength) {
   debug("maxString");
-  if (typeof(string)==="number") return string;
-  if (typeof(maxlength) == 'undefined') maxlength = 30;
-  if (typeof(string)=='undefined') return "";
-  if (typeof(string)=='number') return string;
-  if (!string) return ""; 
+  if (typeof (string) === "number") return string;
+  if (typeof (maxlength) === "undefined") maxlength = 30;
+  if (typeof (string) === "undefined") return "";
+  if (typeof (string) === "number") return string;
+  if (!string) return "";
   var newstring = string;
-  if (typeof(string)=='object') newstring = JSON.stringify(string);
-  if (typeof(string)=='boolean') newstring = string.toString();
+  if (typeof (string) === "object") newstring = JSON.stringify(string);
+  if (typeof (string) === "boolean") newstring = string.toString();
 
   if (newstring.length < maxlength) return newstring;
-  return newstring.substring(0,maxlength)+"...";
+  return newstring.substring(0, maxlength) + "...";
 }
 
-function toPGString(string,count) {
+function toPGString(string, count) {
   let result = "";
   if (!count) count = 1;
-  for (let i=0;i<string.length;i++) {
-    let c = string.substring(i,i+1);
-    if ( (c == "'") && (count === 1)) c = "''";
-    if ( (c == "'") && (count === 2)) c = "''''";
-    //if (c= "\\") c= "\\\\";
+  for (let i = 0; i < string.length; i++) {
+    let c = string.substring(i, i + 1);
+    if ((c === "'") && (count === 1)) c = "''";
+    if ((c === "'") && (count === 2)) c = "''''";
+    // if (c= "\\") c= "\\\\";
     result += c;
   }
   return result;
 }
 
 function linkify(string) {
-  debug('linkify');
+  debug("linkify");
   var result = string.toLowerCase();
-  while (result.indexOf(" ")>=0) {
-    result = result.replace(" ","_");
+  while (result.indexOf(" ") >= 0) {
+    result = result.replace(" ", "_");
   }
   return result;
 }
 
 
 function md_render(text) {
-  if (typeof text == "undefined") text = "";
+  if (typeof text === "undefined") text = "";
   if (text === null) text = "";
 
   // search a free standing link and make a markdown link from it.
-  text = text.replace(/\s(https?:\/\/[^\[\]\(\)\s]*)\s/gi, ' [$1]($1) ');
-  text = text.replace(/^(https?:\/\/[^\[\]\(\)\s]*)\s/gi, '[$1]($1) ');
-  text = text.replace(/\s(https?:\/\/[^\[\]\(\)\s]*$)/gi, ' [$1]($1)');
-  text = text.replace(/^(https?:\/\/[^\[\]\(\)\s]*$)/gi, '[$1]($1)');
+  text = text.replace(/\s(https?:\/\/[^\[\]\(\)\s]*)\s/gi, " [$1]($1) ");
+  text = text.replace(/^(https?:\/\/[^\[\]\(\)\s]*)\s/gi, "[$1]($1) ");
+  text = text.replace(/\s(https?:\/\/[^\[\]\(\)\s]*$)/gi, " [$1]($1)");
+  text = text.replace(/^(https?:\/\/[^\[\]\(\)\s]*$)/gi, "[$1]($1)");
   text = markdown.render(text);
-  while (text.search('<a href=')>=0) {
-    text = text.replace('<a href=','<a target="_blank" href=');
+  while (text.search("<a href=") >= 0) {
+    text = text.replace("<a href=", '<a target="_blank" href=');
   }
 
   return text;
@@ -66,10 +66,10 @@ function md_render(text) {
 var isUrlRegex =      /^(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
 var getAllUrlRegex = /(https?:\/\/[^\[\] \n\r()]*)/g;
 
-//var getAllUrlRegex  = /(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g;
+// var getAllUrlRegex  = /(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g;
 
-//var getAllUrlRegex  = /(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g;
-//var isUrlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+// var getAllUrlRegex  = /(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g;
+// var isUrlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 
 function isURL(t) {
   if (t) return isUrlRegex.test(t);
@@ -77,8 +77,8 @@ function isURL(t) {
 }
 
 function getAllURL(t) {
-  let r =t.match(getAllUrlRegex);
-  if (r===null) return [];
+  let r = t.match(getAllUrlRegex);
+  if (r === null) return [];
   return r;
 }
 // shorten shorten a string up to maxlength
