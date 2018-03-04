9c075c2d66418979e127d6844eebb6bf57a1f4ee
TheFive <TheFive.osm@gmail.com>
2018-03-04 23:24:11 +0100

    Close #749. Shorten links under collection in aritcle editor.

--- test/views.article_new.test.js
+++ test/views.article_new.test.js
@@ -146,13 +146,14 @@ describe("views/article_new", function() {
       browser.assert.text("#collection", "Link1: http://www.test.dä/holla and other");
       return bddone();
     });
-    function checkLink(link,langVisible, langTranslation) {
+    function checkLink(link,langVisible, langTranslation,displayLink) {
       if (!langTranslation) langTranslation = langVisible;
+      if (!displayLink) displayLink = link;
       let transText = "MISSING TRANSTEXT in TEST";
       if (langVisible === "DE") transText = "automatische [Übersetzung]";
       if (langVisible === "EN") transText = "automatic [translation]";
       // Check the visible Link
-      browser.assert.text('#linkArea a[href="'+link+'"]', link);
+      browser.assert.text('#linkArea a[href="'+link+'"]', displayLink);
 
       // Check the translation
       browser.assert.text('#linkArea a[href="https://translate.google.com/translate?sl=auto&tl='+langTranslation+'&u='+link+'"]', langVisible);
@@ -179,11 +180,11 @@ describe("views/article_new", function() {
       // Change collection with two links
       browser.fill("collection", "https://productforums.google.com/forum/#!topic/map-maker/Kk6AG2v-kzE\nhere: http://www.openstreetmap.org/user/Severák/diary/37681");
 
-      checkLink("https://productforums.google.com/forum/#!topic/map-maker/Kk6AG2v-kzE","DE");
-      checkLink("https://productforums.google.com/forum/#!topic/map-maker/Kk6AG2v-kzE","EN");
+      checkLink("https://productforums.google.com/forum/#!topic/map-maker/Kk6AG2v-kzE","DE","DE","https://productforums.google.com/forum/# . . . v-kzE");
+      checkLink("https://productforums.google.com/forum/#!topic/map-maker/Kk6AG2v-kzE","EN","EN","https://productforums.google.com/forum/# . . . v-kzE");
 
-      checkLink("http://www.openstreetmap.org/user/Severák/diary/37681","DE");
-      checkLink("http://www.openstreetmap.org/user/Severák/diary/37681","EN");
+      checkLink("http://www.openstreetmap.org/user/Severák/diary/37681","DE","DE","http://www.openstreetmap.org/user/Severá . . . 37681");
+      checkLink("http://www.openstreetmap.org/user/Severák/diary/37681","EN","EN","http://www.openstreetmap.org/user/Severá . . . 37681");
 
       bddone();
     });
