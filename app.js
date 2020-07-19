2458191f66d00a6e756bce0f617447851d69e4c5
TheFive <thefive.osm@gmail.com>
2020-07-19 13:25:36 +0200

    Added reviews to central blog management page

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ if ((app.get("env") === "test") && (process.env.MOCHA_WITH_MORGAN === "TRUE")) {
 // first register the unsecured path, with no cookie need.
 
 app.use(htmlRoot + "/bower_components/bootstrap", express.static(path.join(__dirname, "/node_modules/bootstrap")));
-app.use(htmlRoot + "/bower_components/font_awesome", express.static(path.join(__dirname, "/node_modules/font_awesome")));
+app.use(htmlRoot + "/bower_components/font-awesome", express.static(path.join(__dirname, "/node_modules/font-awesome")));
 app.use(htmlRoot + "/bower_components/jquery", express.static(path.join(__dirname, "/node_modules/jquery")));
 app.use(htmlRoot + "/bower_components/d3", express.static(path.join(__dirname, "/node_modules/d3")));
 app.use(htmlRoot + "/bower_components/markdown-it", express.static(path.join(__dirname, "/node_modules/markdown-it")));
