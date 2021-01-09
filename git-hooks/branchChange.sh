6768aba54284e2207b1b609115742c1aca45b501
TheFive <thefive.osm@gmail.com>
2021-01-09 16:13:53 +0100

    no message

---
+++ git-hooks/branchChange.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install this file in your .git/hooks files by calling this scripts
+# from a script in the folders
+# .git/hooks/post-merge
+# .git/hooks/post-checkout
+
+changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+
+runOnChange() {
+	echo "$changedFiles" | grep -q "$1" && eval "$2"
+}
+
+runOnChange package-lock.json "npm install"
