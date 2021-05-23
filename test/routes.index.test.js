8a9c1bb57a722ae36850afd43d75f9ceebb167f0
TheFive <thefive.osm@gmail.com>
2021-05-23 20:55:39 +0200

    First Tests complete test migration to bootstrap4

--- test/routes.index.test.js
+++ test/routes.index.test.js
@@ -138,7 +138,7 @@ describe("routes/index", function() {
       let user = await userModule.findById(1);
       should(user).eql({
         id: "1",
-        "langArray": [null,null,null, null],
+        "langArray": ["EN",null,null, null],
         OSMUser: "TestUser",
         access: "full",
         version: 2,
