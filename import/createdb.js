91759dd223472b1be9183a63bdeeb82c1e5a8278
TheFive <thefive.osm@gmail.com>
2022-05-14 17:16:24 +0200

    eslint createdb.js

--- import/createdb.js
+++ import/createdb.js
@@ -1,27 +1,27 @@
 "use strict";
 
-var async   = require('async');
-var should  = require('should');
-var pg      = require('pg');
-var program = require('commander');
-require('colors');
+const async   = require("async");
+const should  = require("should");
+const pg      = require("pg");
+const program = require("commander");
 
 
-var config        = require('../config.js');
-var pgMap         = require('../model/pgMap.js');
-var blogModule    = require('../model/blog.js');
-var articleModule = require('../model/article.js');
-var logModule     = require('../model/logModule.js');
-var userModule    = require('../model/user.js');
-var configModule  = require('../model/config.js');
-var session       = require('../model/session.js');
+
+const config        = require("../config.js");
+const pgMap         = require("../model/pgMap.js");
+const blogModule    = require("../model/blog.js");
+const articleModule = require("../model/article.js");
+const logModule     = require("../model/logModule.js");
+const userModule    = require("../model/user.js");
+const configModule  = require("../model/config.js");
+const session       = require("../model/session.js");
 
 
-var jsdiff = require('diff');
+const jsdiff = require("diff");
 
-function coloredDiffLog(one,other) {
-  should(typeof(one)=="string");
-  should(typeof(other)=="string");
+function coloredDiffLog(one, other) {
+  should(typeof (one) === "string");
+  should(typeof (other) === "string");
 
 
   var diff = jsdiff.diffWords(one, other);
@@ -29,36 +29,36 @@ function coloredDiffLog(one,other) {
   diff.forEach(function (part) {
     // green for additions, red for deletions
     // grey for common parts
-    var color = part.added ? 'green' :
-      part.removed ? 'red' : 'grey';
+    var color = part.added ? "green" :
+      part.removed ? "red" : "grey";
     process.stderr.write(part.value[color]);
   });
   console.info();
 }
 
 program
-  .option('--dropTable [table]','Drop specific table before creation','')
-  .option('--dropTables','Drop Tables before Creation','')
-  .option('--dropIndex','Drop Index before Creation','')
-  .option('--dropView','Drop View before Creation','')
-  .option('--createTables','Create all tables')
-  .option('--createTable [table]','Create a specific table')
-  .option('--createView','Create all Views','')
-  .option('--createIndex','Create all Index','')
-  .option('--verbose','verbose option','')
-  .option('--dd','display table definition from database server','')
-  .option('--updateIndex','bring Indexes updtodate (keep existing)','')
-  .option('--addUser [user]','add user with full access to database','')
+  .option("--dropTable [table]","Drop specific table before creation","")
+  .option("--dropTables","Drop Tables before Creation","")
+  .option("--dropIndex","Drop Index before Creation","")
+  .option("--dropView","Drop View before Creation","")
+  .option("--createTables","Create all tables")
+  .option("--createTable [table]","Create a specific table")
+  .option("--createView","Create all Views","")
+  .option("--createIndex","Create all Index","")
+  .option("--verbose","verbose option","")
+  .option("--dd","display table definition from database server","")
+  .option("--updateIndex","bring Indexes updtodate (keep existing)","")
+  .option("--addUser [user]","add user with full access to database","")
   .parse(process.argv);
 
 if (program.verbose) {
-  console.info("Using NODE_ENV "+config.env);
+  console.info("Using NODE_ENV " + config.env);
 }
 
 
 config.initialise();
 
-if (program.dropTable && process.env.NODE_ENV==="production") {
+if (program.dropTable && process.env.NODE_ENV === "production") {
   console.error("You are going to delete production tables, please do that manual in postgres");
   process.exit();
 }
@@ -78,7 +78,7 @@ var pgOptions = {
 };
 
 
-function analyseTable(pgObject,pgOptions,callback) {
+function analyseTable(pgObject, pgOptions,callback) {
   
   // first copy all expected indexes
   var expected = {};
@@ -87,10 +87,10 @@ function analyseTable(pgObject,pgOptions,callback) {
   var foundUnnecessary = {};
   var database = {};
   for (var k in pgObject.indexDefinition) {
-    expected[k]=pgObject.indexDefinition[k];
+    expected[k] = pgObject.indexDefinition[k];
   }
   pg.connect(config.pgstring,function(err,client,pgdone){
-    var sql = "select indexname,indexdef from pg_indexes where tablename ='"+pgObject.table+"' and indexname not in (select conname from pg_constraint);";
+    var sql = "select indexname,indexdef from pg_indexes where tablename ='" + pgObject.table + "' and indexname not in (select conname from pg_constraint);";
     var query = client.query(sql);
     query.on("err",function(err){
       pgdone();
@@ -99,20 +99,20 @@ function analyseTable(pgObject,pgOptions,callback) {
     query.on("row",function(row){
       var index = row.indexname;
       var indexdef = row.indexdef;
-      database[index]= indexdef;
+      database[index] = indexdef;
 
       if (expected[index]) {
         var diff = jsdiff.diffWords(expected[index],indexdef);
-        if (diff.length==1 && !diff[0].added && !diff[0].removed) {
+        if (diff.length == 1 && !diff[0].added && !diff[0].removed) {
           // index is the same in DB than expected
-          foundOK[index]=expected[index];
+          foundOK[index] = expected[index];
           delete expected[index];
         } else {
-          foundNOK[index]=expected[index];
+          foundNOK[index] = expected[index];
           delete expected[index];
         }
       } else {
-        foundUnnecessary[index]= indexdef;
+        foundUnnecessary[index] = indexdef;
       }
     });
     query.on("end",function(){
@@ -124,26 +124,26 @@ function analyseTable(pgObject,pgOptions,callback) {
         console.info("Checking Table",pgObject.table);
         console.info(" Found indexes OK");
         for (k in foundOK) {
-          console.info("  "+k);
+          console.info("  " + k);
         }
         console.info(" Found indexes Need Update");
         for (k in foundNOK) {
 
-          console.info("  "+k+" Difference:");
+          console.info("  " + k + " Difference:");
           coloredDiffLog(foundNOK[k],database[k]);
         }
         console.info(" Missing indexes");
         for (k in expected) {
-          console.info("  "+k);
+          console.info("  " + k);
           coloredDiffLog(expected[k],"");
         }
         console.info(" Unnecessary indexes");
         for (k in foundUnnecessary) {
-          console.info("  "+k);
+          console.info("  " + k);
           coloredDiffLog("",foundUnnecessary[k]);
         }
       }
-      var result ={
+      var result = {
         foundOK:foundOK,
         foundNOK:foundNOK,
         foundUnnecessary:foundUnnecessary,
