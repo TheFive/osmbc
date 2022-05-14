c60fec8c88c379dfe2d64ef1182d23f8853576d9
TheFive <thefive.osm@gmail.com>
2022-05-14 18:52:12 +0200

    Add Guest for Mail Receiver.

--- util/initialise.js
+++ util/initialise.js
@@ -20,7 +20,7 @@ const slackReceiver  = require("../notification/slackReceiver.js");
 // Initialise Mail Module with all users
 function startMailReceiver(callback) {
   debug("startMailReceiver");
-  userModule.find({ access: "full" }, function initUsers(err, result) {
+  userModule.find({ access: "IN('full','guest')" }, function initUsers(err, result) {
     if (err) {
       return callback(new Error("Error during User Initialising for Mail " + err.message));
     }
