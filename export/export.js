a57a0f6acd1deefe7a635ba7fa6910a54c359fbd
TheFive <thefive.osm@gmail.com>
2022-10-29 12:54:30 +0200

    eslint currently unused import and export files.

--- export/export.js
+++ export/export.js
@@ -1,65 +1,65 @@
 "use strict";
-var assert = require('assert');
+const assert = require("assert");
 
-var fs=require('fs');
+const fs = require("fs");
 
 
-var articleModule = require( "../model/article.js");
-var blogModule = require("../model/blog.js");
-var logModule = require("../model/logModule.js");
-var configModule = require("../model/config.js");
-var async = require('async');
+const articleModule = require("../model/article.js");
+const blogModule = require("../model/blog.js");
+const logModule = require("../model/logModule.js");
+const configModule = require("../model/config.js");
+const async = require("async");
 
-function exportArticle(blog,callback) {
+function exportArticle(blog, callback) {
   console.info("export article");
-  articleModule.find({blog:blog},function(err,result){
+  articleModule.find({ blog: blog }, function(err, result) {
     if (err) return console.error(err);
     if (!result) return console.info("Nothing found");
-    fs.writeFileSync("article.json","[");
-    for (let i=0;i<result.length;i++) {
-      let a=result[i];
+    fs.writeFileSync("article.json", "[");
+    for (let i = 0; i < result.length; i++) {
+      const a = result[i];
       delete a._meta;
-      let s=JSON.stringify(a,null,2);
-      if (i>0) fs.appendFileSync("article.json",",\n");
-      fs.appendFileSync("article.json",s);
+      const s = JSON.stringify(a, null, 2);
+      if (i > 0) fs.appendFileSync("article.json", ",\n");
+      fs.appendFileSync("article.json", s);
     }
-    fs.appendFileSync("article.json","]");
+    fs.appendFileSync("article.json", "]");
     return callback();
   });
 }
 
-function exportBlog(blo,callback) {
+function exportBlog(blo, callback) {
   console.info("export blog");
-  blogModule.find({name:blog},function(err,result){
+  blogModule.find({ name: blog }, function(err, result) {
     if (err) return console.error(err);
     if (!result) return console.info("Nothing found");
-    fs.writeFileSync("blog.json","[");
-    for (let i=0;i<result.length;i++) {
-      let a=result[i];
+    fs.writeFileSync("blog.json", "[");
+    for (let i = 0; i < result.length; i++) {
+      const a = result[i];
       delete a._meta;
-      let s=JSON.stringify(a,null,2);
-      if (i>0) fs.appendFileSync("blog.json",",\n");
-      fs.appendFileSync("blog.json",s);
+      const s = JSON.stringify(a, null, 2);
+      if (i > 0) fs.appendFileSync("blog.json", ",\n");
+      fs.appendFileSync("blog.json", s);
     }
-    fs.appendFileSync("blog.json","]");
+    fs.appendFileSync("blog.json", "]");
     return callback();
   });
 }
 
-function exportChange(blog,callback) {
+function exportChange(blog, callback) {
   console.info("export change");
-  logModule.find({blog:blog},function(err,result){
+  logModule.find({ blog: blog }, function(err, result) {
     if (err) return console.error(err);
     if (!result) return console.info("Nothing found");
-    fs.writeFileSync("change.json","[");
-    for (let i=0;i<result.length;i++) {
-      let a=result[i];
+    fs.writeFileSync("change.json", "[");
+    for (let i = 0; i < result.length; i++) {
+      const a = result[i];
       delete a._meta;
-      let s=JSON.stringify(a,null,2);
-      if (i>0) fs.appendFileSync("change.json",",\n");
-      fs.appendFileSync("change.json",s);
+      const s = JSON.stringify(a, null, 2);
+      if (i > 0) fs.appendFileSync("change.json", ",\n");
+      fs.appendFileSync("change.json", s);
     }
-    fs.appendFileSync("change.json","]");
+    fs.appendFileSync("change.json", "]");
     return callback();
   });
 }
@@ -67,12 +67,12 @@ function exportChange(blog,callback) {
 
 var blog = process.argv[2];
 
-assert(typeof(blog)=="string");
-assert(blog.length>=3);
+assert(typeof (blog) === "string");
+assert(blog.length >= 3);
 
 async.series([
   configModule.initialise,
-  exportArticle.bind(null,blog),
-  exportBlog.bind(null,blog),
-  exportChange.bind(null,blog)
-],function(err){console.error(err);console.info("Fertig");});
+  exportArticle.bind(null, blog),
+  exportBlog.bind(null, blog),
+  exportChange.bind(null, blog)
+], function(err) { console.error(err); console.info("Fertig"); });
--- import/download.js
+++ import/download.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var request = require("request");
-var toMarkdown = require('to-markdown');
+const request = require("request");
+const toMarkdown = require("to-markdown");
 
 // Disable htmlparster as it no longer required in main project
 // this sources are in repository for historical reasons,
@@ -9,201 +9,193 @@ var toMarkdown = require('to-markdown');
 //  htmlparser = require("htmlparser2");
 const htmlparser = null;
 
-var debug = require('debug')('OSMBC:import:download');
+const debug = require("debug")("OSMBC:import:download");
 
-var articleModule = require('../model/article.js');
-var blogModule = require('../model/blog.js');
-var config=require('../config.js');
+const articleModule = require("../model/article.js");
+const blogModule = require("../model/blog.js");
+const config = require("../config.js");
 
-var async = require('async');
+const async = require("async");
 
 
 
 // For Translation:
 
-var categoryTranslation =
+const categoryTranslation =
 {
-  "[Aktuelle Kategorie]":"[Actual Category]",
-  "In eigener Sache":"About us",
-  "Wochenaufruf":"Weekly exerciseEN:",
-  "Mapping":"Mapping",
-  "Community":"Community",
-  "Importe":"Imports",
-  "OpenStreetMap Foundation":"OpenStreetMap Foundation",
-  "Veranstaltungen":"Events",
-  "Humanitarian OSM":"Humanitarian OSM",
-  "Karten":"Maps",
-  "switch2OSM":"#switch2OSM",
-  "Open-Data":"Open Data",
-  "Lizenzen":"Licences",
-  "Programme":"Software",
-  "Programmierung":"Programming",
-  "Kennst Du schon …":"Did you know …",
-  "Weitere Themen mit Geo-Bezug":'Other “geo” things',
-  "Wochenvorschau" :"Weekly Preview",
-  "ODbL":"ODbL",
-  "Humanitarian OpenStreetMap":"Humanitarian OpenStreetMap",
-  "Humanitarian OpenStreetMap Team":"Humanitarian OpenStreetMap Team",
-  "Weltweit":"Global",
-  "Sonstiges":"Other",
-  "Talk, Forum, Wiki & Blog":"Talk, Forum, Wiki & Blog",
-  "Talk, Forum, Wiki":"Talk, Forum, Wiki",
-  "Blog & Twitter":"Blog & Twitter",
-  "Import":"Import",
-  "Tagging":"Tagging",
-  "Open Data & ODbL":"Open Data & ODbL",
-  "Konferenzen & Treffen":"Conferences",
-  "und sonst":"Other",
-  "Karten & Programme":"Maps & Programs",
-  "#switch2osm":"#switch2osm",
-  "Weltweit & sonstiges":"Global & Other"
+  "[Aktuelle Kategorie]": "[Actual Category]",
+  "In eigener Sache": "About us",
+  Wochenaufruf: "Weekly exerciseEN:",
+  Mapping: "Mapping",
+  Community: "Community",
+  Importe: "Imports",
+  "OpenStreetMap Foundation": "OpenStreetMap Foundation",
+  Veranstaltungen: "Events",
+  "Humanitarian OSM": "Humanitarian OSM",
+  Karten: "Maps",
+  switch2OSM: "#switch2OSM",
+  "Open-Data": "Open Data",
+  Lizenzen: "Licences",
+  Programme: "Software",
+  Programmierung: "Programming",
+  "Kennst Du schon …": "Did you know …",
+  "Weitere Themen mit Geo-Bezug": "Other “geo” things",
+  Wochenvorschau: "Weekly Preview",
+  ODbL: "ODbL",
+  "Humanitarian OpenStreetMap": "Humanitarian OpenStreetMap",
+  "Humanitarian OpenStreetMap Team": "Humanitarian OpenStreetMap Team",
+  Weltweit: "Global",
+  Sonstiges: "Other",
+  "Talk, Forum, Wiki & Blog": "Talk, Forum, Wiki & Blog",
+  "Talk, Forum, Wiki": "Talk, Forum, Wiki",
+  "Blog & Twitter": "Blog & Twitter",
+  Import: "Import",
+  Tagging: "Tagging",
+  "Open Data & ODbL": "Open Data & ODbL",
+  "Konferenzen & Treffen": "Conferences",
+  "und sonst": "Other",
+  "Karten & Programme": "Maps & Programs",
+  "#switch2osm": "#switch2osm",
+  "Weltweit & sonstiges": "Global & Other"
 
 };
 
 
 // This is the simple HTML Parser for the WN
 
-var currentCategory = new Category();
-var currentElement = "";
+let currentCategory = new Category();
+let currentElement = "";
 
-var state = null;
-var result = [];
-var categoryList = [];
-
-
-var parser = new htmlparser.Parser({
-    onopentag: function(name, attribs){
-        // ignore everything in comment mode
-        if (state == "finished") return;
-
-        if(name == "h2" ){
-            state = "category";
-
-        }
-        if (name == "li") {
-          state = "list";
-        }
-        if (name == "a" && state == "list") {
-            currentElement += "<a href=\""+attribs.href+"\">";
-
-        }
-        if (name == "div") {
-          if (attribs.id == "entry-author-info") {
-            state = "finished";
-            result.push(currentCategory);
-          }
-        }
-    },
-    ontext: function(text){
-        if (state == "finished") return;
+let state = null;
+let result = [];
+let categoryList = [];
 
 
-        if (state == "category" && text != currentCategory.title) {
-          text = text.replace("&amp;","&");
-          result.push(currentCategory);
-          currentCategory =new Category();
-          currentCategory.title = text;
-          categoryList.push(text);
-        }
-        if (state == "list") {
-          currentElement += text;
-        }
+const parser = new htmlparser.Parser({
+  onopentag: function(name, attribs) {
+    // ignore everything in comment mode
+    if (state === "finished") return;
 
-
-    },
-    onclosetag: function(tagname){
-        if (state == "finished") return;
-        if(tagname === "h2"){
-           state = "";
-        }
-        if(tagname === "li"){
-           state = "";
-           currentCategory.element.push(currentElement);
-           currentElement = "";
-        }
-        if (tagname == "a" && state == "list") {
-            currentElement += "</a>";
-
-        }
-
+    if (name == "h2") {
+      state = "category";
     }
-}, {decodeEntities: false});
+    if (name == "li") {
+      state = "list";
+    }
+    if (name == "a" && state == "list") {
+      currentElement += "<a href=\"" + attribs.href + "\">";
+    }
+    if (name == "div") {
+      if (attribs.id == "entry-author-info") {
+        state = "finished";
+        result.push(currentCategory);
+      }
+    }
+  },
+  ontext: function(text) {
+    if (state == "finished") return;
+
+
+    if (state == "category" && text != currentCategory.title) {
+      text = text.replace("&amp;", "&");
+      result.push(currentCategory);
+      currentCategory = new Category();
+      currentCategory.title = text;
+      categoryList.push(text);
+    }
+    if (state == "list") {
+      currentElement += text;
+    }
+  },
+  onclosetag: function(tagname) {
+    if (state == "finished") return;
+    if (tagname === "h2") {
+      state = "";
+    }
+    if (tagname === "li") {
+      state = "";
+      currentCategory.element.push(currentElement);
+      currentElement = "";
+    }
+    if (tagname == "a" && state == "list") {
+      currentElement += "</a>";
+    }
+  }
+}, { decodeEntities: false });
 
 
 function Category() {
-    this.title = null;
-    this.element = [];
-  }
+  this.title = null;
+  this.element = [];
+}
 
-function importBlog(nr,callback) {
-  debug('importBlog %s',nr);
+function importBlog(nr, callback) {
+  debug("importBlog %s", nr);
 
-  var number = "000"+nr;
+  const number = "000" + nr;
 
-  var blogName = "WN"+number.substring(number.length-3,99);
-  console.info("Import WN "+blogName);
+  const blogName = "WN" + number.substring(number.length - 3, 99);
+  console.info("Import WN " + blogName);
 
-  blogModule.findOne({name:blogName},function (err,b) {
-  if (b) {
-    console.error(blogName+" already exists");
-    return callback();
-  }
-
-  var body = null;
-  var url = null;
-  async.parallel([
-    function tryOneName(cb) {
-      debug('tryOneName');
-      var u = "http://blog.openstreetmap.de/blog/wochennotiz-nr-"+nr;
-      request(u, function (error, response, b) {
-        if (!error) {
-          debug('Found URL %s',u);
-          body = b;
-          url = u;
-        } else {
-          debug('Not found URL %s',u);
-        }
-        cb();
-      });
-    },
-    function tryOtherName(cb) {
-      debug('tryOtherName');
-      var u = "http://blog.openstreetmap.de/blog/osm-wochennotiz-nr-"+nr;
-      request(u, function (error, response, b) {
-        if (!error) {
-          debug('Found URL %s',u);
-          body = b;
-          url = u;
-        } else {
-          debug('Not found URL %s',u);
-        }
-        cb();
-      });
+  blogModule.findOne({ name: blogName }, function (err, b) {
+    if (b) {
+      console.error(blogName + " already exists");
+      return callback();
     }
 
+    let body = null;
+    let url = null;
+    async.parallel([
+      function tryOneName(cb) {
+        debug("tryOneName");
+        const u = "http://blog.openstreetmap.de/blog/wochennotiz-nr-" + nr;
+        request(u, function (error, response, b) {
+          if (!error) {
+            debug("Found URL %s", u);
+            body = b;
+            url = u;
+          } else {
+            debug("Not found URL %s", u);
+          }
+          cb();
+        });
+      },
+      function tryOtherName(cb) {
+        debug("tryOtherName");
+        const u = "http://blog.openstreetmap.de/blog/osm-wochennotiz-nr-" + nr;
+        request(u, function (error, response, b) {
+          if (!error) {
+            debug("Found URL %s", u);
+            body = b;
+            url = u;
+          } else {
+            debug("Not found URL %s", u);
+          }
+          cb();
+        });
+      }
+
     ],
-    function loadedWN(err){
-      debug('loadedWN');
+    function loadedWN(err) {
+      debug("loadedWN");
       if (err) console.error(err);
       if (body) {
         state = null;
         result = [];
         categoryList = [];
         parser.write(body);
-        console.info("Loaded Url:",url);
-        var blog = blogModule.create();
+        console.info("Loaded Url:", url);
+        const blog = blogModule.create();
 
-        //fs.writeFileSync(blogName,body,"utf8");
+        // fs.writeFileSync(blogName,body,"utf8");
 
-        var cat =[];
-        for (var i=0;i<categoryList.length;i++) {
-          var c = {DE:categoryList[i]};
+        const cat = [];
+        for (let i = 0; i < categoryList.length; i++) {
+          const c = { DE: categoryList[i] };
           if (c.DE in categoryTranslation) {
             c.EN = categoryTranslation[c.DE];
-          }
-          else
-          {
-            c.EN = c.DE+"Not Translated";
+          } else {
+            c.EN = c.DE + "Not Translated";
             console.info(c.EN);
           }
           cat.push(c);
@@ -211,51 +203,44 @@ function importBlog(nr,callback) {
 
 
 
-        blog.setAndSave({OSMUser:"IMPORT"},{name:blogName,status:"published",categories:cat},function(err){
-          console.info("Blog Saved Error("+err+")");
-          async.each(result,function iterator(item,cb) {
-            var category = item.title;
-            //console.info("ITEM"+item);
+        blog.setAndSave({ OSMUser: "IMPORT" }, { name: blogName, status: "published", categories: cat }, function(err) {
+          console.info("Blog Saved Error(" + err + ")");
+          async.each(result, function iterator(item, cb) {
+            const category = item.title;
+            // console.info("ITEM"+item);
             if (category === null) return cb();
-            //console.info("Import Category: "+category);
-            //console.info(item.element.length+" Elements in Category");
-            async.each(item.element,function eachArticle(articleProto,cba) {
-              //console.info(articleProto);
-              var markdownDE = toMarkdown(articleProto);
-              var article = articleModule.create();
-              article.setAndSave({OSMUser:"IMPORT"},{blog:blogName,category:category,markdownDE:markdownDE},function(err){
+            // console.info("Import Category: "+category);
+            // console.info(item.element.length+" Elements in Category");
+            async.each(item.element, function eachArticle(articleProto, cba) {
+              // console.info(articleProto);
+              const markdownDE = toMarkdown(articleProto);
+              const article = articleModule.create();
+              article.setAndSave({ OSMUser: "IMPORT" }, { blog: blogName, category: category, markdownDE: markdownDE }, function(err) {
                 cba(err);
               });
-
-
-            }, function done() {cb();}
+            }, function done() { cb(); }
             );
-
-
-
           }, function() {
-            console.info("Blog "+blogName+ " imported");
+            console.info("Blog " + blogName + " imported");
             callback();
           }
-          ) ;
+          );
         });
       }
     });
-
-
   });
 }
 
 
 config.initialise();
 
-importBlog(268,function(){console.info("Ready.");});
-importBlog(269,function(){console.info("Ready.");});
-importBlog(270,function(){console.info("Ready.");});
-importBlog(271,function(){console.info("Ready.");});
+importBlog(268, function() { console.info("Ready."); });
+importBlog(269, function() { console.info("Ready."); });
+importBlog(270, function() { console.info("Ready."); });
+importBlog(271, function() { console.info("Ready."); });
 /*
 async.timesLimit(1,1,function (n,next){
   console.info("Start Import for "+n);
   importBlog(n,next);
 
-})*/
+}) */
