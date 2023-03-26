0f7ffbc5c52f299a180b4843d5579fc7056371d1
TheFive <thefive.osm@gmail.com>
2023-03-26 18:20:57 +0200

    Fix some test for #980

--- public/javascripts/article_view.js
+++ public/javascripts/article_view.js
@@ -146,7 +146,7 @@ function getEventTable(lang, edit, pressedButton) {
 
   $.ajax({
     url: url,
-    data: { startDate: window.blogStartDate },
+    data: { blogStartDate: window.blogStartDate },
     method: "POST",
     success: function(data) {
       if (data) $(edit).val(data).trigger("change");
