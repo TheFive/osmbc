d28913ef39348c4e8c3d3043ae9e6262b6766cb5
TheFive <thefive.osm@gmail.com>
2026-07-12 15:58:19 +0200

    place config.getValue in static area to check mustExist at start.

--- render/HugoMarkdownRenderer.js
+++ render/HugoMarkdownRenderer.js
@@ -8,7 +8,8 @@ import { turndownService , osmbcMarkdown } from "../util/md_util.js";
 
 const debug = _debug("OSMBC:render:HugoMarkdownRenderer");
 
-const categoryTranslation = configModule.getConfig("categorytranslation");
+const wpExpressTitle = config.getValue("Blog Title For Export", { mustExist: true });
+const dateAdjust = Number(config.getValue("Hugo", "DateAdjust", { mustExist: true }));
 
 
 class HugoMarkdownRenderer extends MarkdownRenderer {
@@ -87,8 +88,7 @@ class HugoMarkdownRenderer extends MarkdownRenderer {
   _generateFrontText(lang, pictureArticles) {
     // generate TOML header for Hugo front matter
     debug("HugoMarkdownRenderer.prototype._generateFrontText %s", lang);
-    const wpExpressTitle = config.getValue("Blog Title For Export", { mustExist: true });
-    const dateAdjust = Number(config.getValue("Hugo", "DateAdjust", { mustExist: true }));
+    const categoryTranslation = configModule.getConfig("categorytranslation");
 
     const blogNames = (categoryTranslation.filter((category) => { return (category.EN === wpExpressTitle); }))[0];
     const date = moment(this.blog.endDate).tz("Europe/Berlin").add(dateAdjust, "days").format("YYYY-MM-DD");
